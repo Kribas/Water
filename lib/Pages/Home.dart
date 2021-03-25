87bc122006d17bf0612b2674866a5006bc1cebe0
Kribas Rimal <carlousrimal@gmail.com>
2021-03-25 12:08:16 +0545

    made changes

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,6 +1,7 @@
 import 'package:drpani/Components/ImageCarousel.dart';
 import 'package:drpani/Components/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -11,14 +12,117 @@ class Home extends StatelessWidget {
       ),
 
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          height: 200,
-          width: double.infinity,
-          child: ImageCarousel(),
-        ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: ImageCarousel(),
+            ),
+          ),
+
+          Divider(),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        radius: 30.0,
+                          child: Icon(Icons.store,
+                          color: Colors.white,
+                            size: 35,
+                          ),
+                      ),
+
+                      SizedBox(height: 5,),
+
+
+                      Text('Shop Now'),
+                    ],
+                  ),
+                ),
+
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        radius: 30.0,
+                        child: Icon(FontAwesomeIcons.mapMarked,
+                        size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      SizedBox(height: 5,),
+
+
+                      Text('Track Order'),
+                    ],
+                  ),
+                ),
+
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        radius: 30.0,
+                        child: Icon(FontAwesomeIcons.gift,
+                        size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      SizedBox(height: 5,),
+
+
+                      Text('My Rewards'),
+                    ],
+                  ),
+                ),
+
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        radius: 30.0,
+                        child: Icon(FontAwesomeIcons.comment,
+                        size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      SizedBox(height: 5,),
+
+                      Text('Feedback'),
+
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          Divider(),
+
+        ],
       ),
+
     );
   }
 }
